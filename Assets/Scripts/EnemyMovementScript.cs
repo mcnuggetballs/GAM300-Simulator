5689cdd782f17a561afd278a2e1057dd70803695
JTZQ <fishyweeb@gmail.com>
2024-09-15 19:42:57 +0800

    enemy follow player

---
+++ Assets/Scripts/EnemyMovementScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovementScript : MonoBehaviour
+{
+    [SerializeField]
+    Entity thePlayer;
+    EnemyController theEnemy;
+    private void Awake()
+    {
+        theEnemy = GetComponent<EnemyController>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (thePlayer)
+        {
+            Vector3 playerPos = thePlayer.transform.position;
+            playerPos.y = 0;
+            Vector3 enemyPos = transform.position;
+            enemyPos.y = 0;
+            theEnemy.Move((playerPos - enemyPos).normalized, theEnemy.MoveSpeed);
+        }
+    }
+}
