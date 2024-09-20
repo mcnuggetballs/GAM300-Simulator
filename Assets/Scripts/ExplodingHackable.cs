2e6d86ad228cacffbc93a1e5f586fc22f99c8fff
JTZQ <fishyweeb@gmail.com>
2024-09-20 18:49:33 +0800

    explosion damage tweak

--- Assets/Scripts/ExplodingHackable.cs
+++ Assets/Scripts/ExplodingHackable.cs
@@ -6,11 +6,47 @@ public class ExplodingHackable : Hackable
 {
     [SerializeField]
     GameObject explosionPrefab;
+    [SerializeField]
+    Transform explosionPoint;
+
+    public bool explodeSelf;
+    public float selfExplosionForce = 500f;
+    public float selfTorqueForce = 100f;
     public override void Hack(Entity player)
     {
-        Instantiate(explosionPrefab,transform.position,Quaternion.identity,null);
+        Instantiate(explosionPrefab, explosionPoint.position,Quaternion.identity,null);
+        if (explodeSelf)
+        {
+            if (GetComponent<Rigidbody>())
+            {
+                Vector3 domeDirection = GetRandomDirectionInDome();
+                GetComponent<Rigidbody>().AddForce(domeDirection * selfExplosionForce, ForceMode.Impulse);
+                Vector3 randomTorque = GetRandomTorque();
+                GetComponent<Rigidbody>().AddTorque(randomTorque * selfTorqueForce, ForceMode.Impulse);
+            }
+        }
         Destroy(this);
     }
+    private Vector3 GetRandomDirectionInDome()
+    {
+        // Generate a random point on the unit sphere
+        Vector3 randomDirection = Random.onUnitSphere;
 
+        // Force the y-component to be positive (above the XZ plane) to simulate a dome
+        if (randomDirection.y < 0)
+        {
+            randomDirection.y *= -1;
+        }
 
+        return randomDirection.normalized;
+    }
+    private Vector3 GetRandomTorque()
+    {
+        // Generate random torque by creating a random vector
+        return new Vector3(
+            Random.Range(-1f, 1f),
+            Random.Range(-1f, 1f),
+            Random.Range(-1f, 1f)
+        ).normalized;
+    }
 }
--- Assets/True Explosions/System/Scripts/exploders/Exploder.cs
+++ Assets/True Explosions/System/Scripts/exploders/Exploder.cs
@@ -1,89 +1,134 @@
 ﻿using UnityEngine;
-using System.Collections.Generic;
 using System.Collections;
 
-public class Exploder : MonoBehaviour {
+public class Exploder : MonoBehaviour
+{
+    public float explosionTime = 0;
+    public float randomizeExplosionTime = 0;
+    public float radius = 15;
+    public float power = 1;
+    public int probeCount = 150;
+    public float explodeDuration = 0.5f;
+    public LayerMask layerMask; // Add a LayerMask to filter the layers
+    protected bool exploded = false;
 
-	public float explosionTime = 0;
-	public float randomizeExplosionTime = 0;
-	public float radius = 15;
-	public float power = 1;
-	public int probeCount = 150;
-	public float explodeDuration = 0.5f;
+    public virtual IEnumerator explode()
+    {
+        ExploderComponent[] components = GetComponents<ExploderComponent>();
+        foreach (ExploderComponent component in components)
+        {
+            if (component.enabled)
+            {
+                component.onExplosionStarted(this);
+            }
+        }
+        disableCollider();
+        for (int i = 0; i < probeCount; i++)
+        {
+            shootFromCurrentPosition(i);
+        }
+        enableCollider();
+        yield return new WaitForFixedUpdate();
+    }
 
-	protected bool exploded = false;
-	
-	public virtual IEnumerator explode() {
-		ExploderComponent[] components = GetComponents<ExploderComponent>(); 
-		foreach (ExploderComponent component in components) {
-			if (component.enabled) {
-				component.onExplosionStarted(this);
-			}
-		}		
-		while (explodeDuration > Time.time - explosionTime) {
-			disableCollider();
-			for (int i = 0; i < probeCount; i++) {
-				shootFromCurrentPosition();
-			}
-			enableCollider();
-			yield return new WaitForFixedUpdate();
-		}
-	}
-	
-	protected virtual void shootFromCurrentPosition() {
-		Vector3 probeDir = Random.onUnitSphere;
-		Ray testRay = new Ray(transform.position, probeDir);
-		shootRay(testRay, radius);
-	}
+    protected virtual void shootFromCurrentPosition()
+    {
+        Vector3 probeDir = Random.onUnitSphere;
+        Ray testRay = new Ray(transform.position, probeDir);
+        shootRay(testRay, radius);
+    }
 
-	protected bool wasTrigger;
-	public virtual void disableCollider() {
-		if (GetComponent<Collider>()) {
-			wasTrigger = GetComponent<Collider>().isTrigger;
-			GetComponent<Collider>().isTrigger = true;
-		}
-	}
+    protected virtual void shootFromCurrentPosition(int i)
+    {
+        Ray testRay = new Ray(transform.position, GetUniformDirection(i,probeCount));
+        shootRay(testRay, radius);
+    }
 
-	public virtual void enableCollider() {
-		if (GetComponent<Collider>()) {
-			GetComponent<Collider>().isTrigger = wasTrigger;
-		}
-	}
+    protected bool wasTrigger;
+    public virtual void disableCollider()
+    {
+        if (GetComponent<Collider>())
+        {
+            wasTrigger = GetComponent<Collider>().isTrigger;
+            GetComponent<Collider>().isTrigger = true;
+        }
+    }
 
-	
-	protected virtual void init() {
-		power *= 500000;
-		
-		if (randomizeExplosionTime > 0.01f) {
-			explosionTime += Random.Range(0.0f, randomizeExplosionTime);
-		}
-	}
+    public virtual void enableCollider()
+    {
+        if (GetComponent<Collider>())
+        {
+            GetComponent<Collider>().isTrigger = wasTrigger;
+        }
+    }
 
-	void Start() {
-		init();
-	}
+    protected virtual void init()
+    {
+        power *= 500000;
 
-	void FixedUpdate() {
-		if (Time.time > explosionTime && !exploded) {
-			exploded = true;
-			StartCoroutine("explode");
-		}
-	}
+        if (randomizeExplosionTime > 0.01f)
+        {
+            explosionTime += Random.Range(0.0f, randomizeExplosionTime);
+        }
+    }
 
-	private void shootRay(Ray testRay, float estimatedRadius) {
-		RaycastHit hit;
-		if (Physics.Raycast(testRay, out hit, estimatedRadius)) {
-			if (hit.rigidbody != null) {
-				hit.rigidbody.AddForceAtPosition(power * Time.deltaTime * testRay.direction / probeCount, hit.point);
-				estimatedRadius /= 2;
-			} else {
-				Vector3 reflectVec = Random.onUnitSphere;
-				if (Vector3.Dot(reflectVec, hit.normal) < 0) {
-					reflectVec *= -1;
-				}
-				Ray emittedRay = new Ray(hit.point, reflectVec);
-				shootRay(emittedRay, estimatedRadius - hit.distance);
-			}
-		}
-	}
+    void Start()
+    {
+        init();
+    }
+
+    void FixedUpdate()
+    {
+        if (Time.time > explosionTime && !exploded)
+        {
+            exploded = true;
+            StartCoroutine("explode");
+        }
+    }
+
+    private void shootRay(Ray testRay, float estimatedRadius)
+    {
+        RaycastHit hit;
+        // Use the layerMask to filter the raycast
+        if (Physics.Raycast(testRay, out hit, estimatedRadius, layerMask))
+        {
+            Entity theEntity = hit.collider.GetComponent<Entity>();
+            if (hit.rigidbody != null || theEntity)
+            {
+                if (theEntity != null)
+                {
+                    theEntity.TakeDamage(15);
+                }
+                if (hit.rigidbody)
+                {
+                    hit.rigidbody.AddForceAtPosition(power * Time.deltaTime * testRay.direction / probeCount, hit.point);
+                    estimatedRadius /= 2;
+                }
+
+            }
+            else
+            {
+                Debug.LogError(hit.collider);
+                Vector3 reflectVec = Random.onUnitSphere;
+                if (Vector3.Dot(reflectVec, hit.normal) < 0)
+                {
+                    reflectVec *= -1;
+                }
+                Ray emittedRay = new Ray(hit.point, reflectVec);
+                shootRay(emittedRay, estimatedRadius - hit.distance);
+            }
+        }
+    }
+
+    private Vector3 GetUniformDirection(int i, int totalCount)
+    {
+        float phi = Mathf.Acos(1 - 2 * (i + 0.5f) / totalCount);
+        float theta = Mathf.PI * (1 + Mathf.Sqrt(5)) * i;
+
+        float x = Mathf.Sin(phi) * Mathf.Cos(theta);
+        float y = Mathf.Sin(phi) * Mathf.Sin(theta);
+        float z = Mathf.Cos(phi);
+
+        return new Vector3(x, y, z).normalized;
+    }
 }
