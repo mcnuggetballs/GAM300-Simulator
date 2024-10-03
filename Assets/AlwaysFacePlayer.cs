9836964e885ec7558e3ca530274dcad6b14f50b1
bobagummy <122760947+bobagummy@users.noreply.github.com>
2024-10-03 23:15:23 +0800

    mini tutorial prefab

---
+++ Assets/AlwaysFacePlayer.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlwaysFacePlayer : MonoBehaviour
+{
+    void Update()
+    {
+        this.transform.forward = transform.position - Camera.main.transform.position;
+    }
+}
