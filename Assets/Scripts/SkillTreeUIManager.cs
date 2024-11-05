e22fbb202368019029d78e4317a64fec0b9082c6
JTZQ <fishyweeb@gmail.com>
2024-11-06 03:00:42 +0800

    fix

--- Assets/Scripts/SkillTreeUIManager.cs
+++ Assets/Scripts/SkillTreeUIManager.cs
@@ -7,21 +7,17 @@ public class SkillTreeUIManager : MonoBehaviour
     [SerializeField]
     GameObject skillTree;
     [SerializeField]
-    TMPro.TextMeshProUGUI theText;
-    [SerializeField]
     TMPro.TextMeshProUGUI orbCount;
     public void Toggle()
     {
         if (skillTree.activeSelf)
         {
             skillTree.SetActive(false);
-            theText.text = "Skill Tree";
         }
         else
         {
             skillTree.SetActive(true);
             SkillTree.Instance.currentSelectedSkill = null;
-            theText.text = "Close";
         }
     }
 
