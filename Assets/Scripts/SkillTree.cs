6abf719c38249b0da7e230642de3c1977d804236
JTZQ <fishyweeb@gmail.com>
2024-11-06 00:22:45 +0800

    skill tree in progress

--- Assets/Scripts/SkillTree.cs
+++ Assets/Scripts/SkillTree.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class SkillTree : MonoBehaviour
 {
     public SkillTreeImageSwap currentSelectedSkill;
 
     public GameObject outline;
+    public TMPro.TextMeshProUGUI desc;
     public static SkillTree Instance { get; private set; }
     private void Awake()
     {
@@ -18,10 +21,18 @@ public class SkillTree : MonoBehaviour
         {
             outline.SetActive(true);
             outline.transform.position = currentSelectedSkill.transform.position;
+            desc.text = currentSelectedSkill.description;
         }
         else
         {
             outline.SetActive(false);
+        }
+    }
+    public void PurchaseButton()
+    {
+        if (currentSelectedSkill != null)
+        {
+            currentSelectedSkill.Buy();
         }
     }
 }
