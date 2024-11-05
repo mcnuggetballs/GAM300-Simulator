125c28448d0ef74d898cc36e3fa5b9126475cb69
JTZQ <fishyweeb@gmail.com>
2024-11-05 19:55:50 +0800

    fix

--- Assets/StopVelocity.cs
+++ Assets/StopVelocity.cs
@@ -1,3 +1,4 @@
+using StarterAssets;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,10 +6,13 @@ using UnityEngine;
 public class StopVelocity : StateMachineBehaviour
 {
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (animator.GetComponent<ThirdPersonControllerRB>())
+        {
+            animator.GetComponent<ThirdPersonControllerRB>().StopMovement();
+        }
+    }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
